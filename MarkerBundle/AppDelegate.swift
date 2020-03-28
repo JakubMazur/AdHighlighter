152c7b609635a0caef3b5b44bf549bda59dd761c
Jakub Mazur <me@jkmazur.pl>
2020-03-28 12:02:03 +0100

    Initial Commit

---
+++ MarkerBundle/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MarkerBundle
+//
+//  Created by Jakub Mazur on 3/28/20.
+//  Copyright © 2020 Jakub Mazur. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+

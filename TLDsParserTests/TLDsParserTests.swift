ce135e53029401332633c2608efa7009f814ab71
Jakub Mazur <me@jkmazur.pl>
2020-04-05 10:55:49 +0200

    cleanup
    update app icon
    
    complete extension

---
+++ TLDsParserTests/TLDsParserTests.swift
@@ -0,0 +1,18 @@
+//
+//  TLDsParserTests.swift
+//  TLDsParserTests
+//
+//  Created by Jakub Mazur on 4/20/20.
+//  Copyright © 2020 Jakub Mazur. All rights reserved.
+//
+
+import XCTest
+@testable import TLDsParser
+
+class TLDsParserTests: XCTestCase {
+
+    func testParseTLDs() {
+        TLDsParsing.parseTLDs()
+    }
+
+}
